a1972d38a0817ccd335982ac95bd05dd6b56d975
Emmanuel .A <asaoluemma@gmail.com>
2026-01-15 14:03:00 +0100

    initial commit

---
+++ src/asteria/offchain.ts
@@ -0,0 +1,7 @@
+import { MeshTxInitiator, MeshTxInitiatorInput } from "../common";
+
+export class MeshAsteriaContract extends MeshTxInitiator {
+  constructor(inputs: MeshTxInitiatorInput) {
+    super(inputs);
+  }
+}
